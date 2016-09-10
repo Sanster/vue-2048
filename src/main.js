1e3bac7ef079cc8c5b6fbd53cc0985c8f7f7624b
Sanster <cwq1913@163.com>
2016-09-10 21:09:58 +0800

    Init background grid

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
-import Game from './Game.vue'
 require('./style/game.scss')
 
 new Vue({
-  el: 'body',
-  components: { Game }
+	el: '#game',
+	data: {
+		grids: [],
+	},
+	ready: function() {
+	    this.init();
+	},
+	methods: {
+		init: function() {
+		    this.initArrayGrid(4);
+		},
+		initArrayGrid: function(size) {
+		    var arr = [];
+
+		    for (var x = 0; x < size; x++) {
+		      arr[x] = [];
+		      for (var y = 0; y < size; y++) {
+		        arr[x][y] = 0;
+		      }
+		    }
+
+		    this.grids = arr;
+		},
+	}
 })
