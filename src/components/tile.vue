5ccfc7d0b919d528ee8921b5d1f3c2c1b4de027a
Sanster <cwq1913@163.com>
2016-09-11 10:28:31 +0800

    add random init tile

---
+++ src/components/tile.vue
@@ -0,0 +1,33 @@
+<template>
+	<div
+		class="tile tile-{{tile.value}} tile-new{{merged ? ' tile-merged' : ''}}"
+		:style="{transform: 'translate(' + calcStyleX + 'px,' + calcStyleY + 'px)'}"
+		>
+		<div class="tile-inner">{{tile.value}}</div>
+	</div>
+</template>
+
+<script>
+export default {
+	replace: true,
+	props: ['tile'],
+	template: '#tile-template',
+	computed: {
+		calcStyleX: function() {
+			var tilePosition = this.$parent.tilePosition;
+			
+			return tilePosition * this.tile.x;
+		},
+
+		calcStyleY: function() {
+			var tilePosition = this.$parent.tilePosition;
+
+			return tilePosition * this.tile.y;
+		},
+
+		isMerged: function(){
+			return 'tile-merged';
+		}
+	}
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 import Vue from 'vue'
+import Tile from './components/tile.vue'
 require('./style/game.scss')
 
 new Vue({
 	el: '#game',
 	data: {
 		grids: [],
+		tiles: [],
+		startTiles: 2,
+		tilePosition: 121,
+		conf: {
+			score: 0,
+			size: 4,
+			bestScore: 0
+		}
 	},
 	ready: function() {
 	    this.init();
 	},
 	methods: {
 		init: function() {
-		    this.initArrayGrid(4);
+		    this.initArrayGrid(this.conf.size);
+
+	    	for (var i = 0; i < this.startTiles; i++) {
+				this.addRandomTile();
+			}
 		},
 		initArrayGrid: function(size) {
 		    var arr = [];
@@ -25,5 +38,62 @@ new Vue({
 
 		    this.grids = arr;
 		},
-	}
+		addRandomTile: function() {
+			if (this.availableCells().length > 0) {
+				var value = Math.random() < 0.6 ? 2 : 4;
+
+				var randomCell = this.randomAvailableCell();
+
+				this.addTile({
+					x: randomCell.x,
+					y: randomCell.y,
+					value: value,
+					merged: false
+				});
+			}
+		},
+		addTile: function(tile) {
+
+			var tiles = this.tiles,
+				len = tiles.length;
+
+			tiles.$set(len, {
+				x: tile.x,
+				y: tile.y,
+				value: tile.value,
+				merged: tile.merged,
+			});
+
+			this.grids[tile.x][tile.y] = 1;
+		},
+		availableCells: function() {
+			var cells = [],
+				size = this.conf.size,
+				grids = this.grids;
+
+			for (var x = 0; x < size; x++) {
+				for (var y = 0; y < size; y++) {
+					if (!grids[x][y]) {
+						cells.push({
+							x: x,
+							y: y
+						});
+					}
+				}
+			}
+
+			return cells;
+		},
+		// Find the first available random position
+		randomAvailableCell: function() {
+			var cells = this.availableCells();
+
+			if (cells.length) {
+				return cells[Math.floor(Math.random() * cells.length)];
+			}
+		},
+	},
+	components: {
+		'tile': Tile,
+	},
 })
