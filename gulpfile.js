45d58deb15dcf285a34720137ec87b8a2de4f0ca
Sanster <cwq1913@163.com>
2016-09-16 21:21:34 +0800

    add gulp task to npm build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ gulp.task('default', function() {
   // place code for your default task here
 });
 
+// 执行文件拷贝
 gulp.task('toBlog', function() {
    gulp.src(['./index.html'])
    .pipe(gulp.dest('/Users/cwq/Github/blog/source/projects/vue2048'));
